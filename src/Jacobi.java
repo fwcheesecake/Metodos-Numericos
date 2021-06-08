68d3da95268cbc7440040e387eeb24b68875e69b
fwcheesecake <antoniore102001@outlook.com>
2021-06-07 20:50:00 -0500

    Jacobi Method

--- src/Jacobi.java
+++ src/Jacobi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GauusSeddel {
+public class Jacobi {
     public static void resolve() {
         Scanner sc = new Scanner(System.in);
 
@@ -13,39 +13,40 @@ public class GauusSeddel {
 
         System.out.println("i\t\tx\t\t\tex\t\t\ty\t\t\tey\t\t\tz\t\t\tez");
         int i = 1;
-        while((ex >= 1.0 || ey >= 1.0 || ez >= 1.0) /*&& i <= 10*/) {
+        while((ex >= 1.0 || ey >= 1.0 || ez >= 1.0) && i <= 22) {
             xa = x;
             ya = y;
             za = z;
 
-            x = cut(fx(y, z));
-            y = cut(fy(x, z));
-            z = cut(fz(x, y));
+            x = cut(fx(ya, za));
+            y = cut(fy(xa, za));
+            z = cut(fz(xa, ya));
 
             ex = cut(error(x, xa));
             ey = cut(error(y, ya));
             ez = cut(error(z, za));
 
-            System.out.printf("%d\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f\n",
+            System.out.printf("%d\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\n",
                     (i++), x, ex, y, ey, z, ez);
         }
     }
 
     private static double error(double xr_ac, double xr_an) {
-        double ret = Math.abs((xr_ac - xr_an) / xr_ac * 100.0);
+        final double percentage = 100;
+        double ret = Math.abs((xr_ac - xr_an) / xr_ac * percentage);
         return ret;
     }
 
     private static double fx(double y, double z){
-        return (16.0 - 2.0 * y - 3.0 * z) / 9.0;
+        return (16 - 2 * y - 3 * z) / 9.0;
     }
 
     private static double fy(double x, double z){
-        return (142.0 - 4.0 * x - 8.0 * z) / 10.0;
+        return (142 - 4 * x - 8 * z) / 10.0;
     }
 
     private static double fz(double x, double y){
-        return (13.0 - 2.0 * x - 6.0 * y) / 7.0;
+        return (13 - 2 * x - 6 * y) / 7.0;
     }
 
     private static double cut(double x) {
